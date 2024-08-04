b9d498c41483ab81df19af06062731394c423e21
eeddy <eeddy@unb.ca>
2024-08-04 13:02:58 -0300

    Updated for new version of LibEMG

--- main_menu.py
+++ main_menu.py
@@ -1,20 +1,18 @@
 from tkinter import *
 from myo_mouse import MyoMouse
-from libemg.screen_guided_training import ScreenGuidedTraining
-from libemg.data_handler import OnlineDataHandler, OfflineDataHandler
+from libemg.gui import GUI
+from libemg.data_handler import OnlineDataHandler, OfflineDataHandler, RegexFilter
 from libemg.streamers import myo_streamer
-from libemg.utils import make_regex
 from libemg.feature_extractor import FeatureExtractor
-from libemg.emg_classifier import OnlineEMGClassifier, EMGClassifier
+from libemg.emg_predictor import OnlineEMGClassifier, EMGClassifier
 
 class Menu:
     def __init__(self):
         # Myo Streamer - start streaming the myo data 
-        myo_streamer()
+        self.streamer, sm = myo_streamer()
 
         # Create online data handler to listen for the data
-        self.odh = OnlineDataHandler()
-        self.odh.start_listening()
+        self.odh = OnlineDataHandler(sm)
 
         self.classifier = None
         self.proportional = None
@@ -43,14 +41,15 @@ class Menu:
     def start_mouse(self):
         self.window.destroy()
         self.set_up_classifier()
-        MyoMouse(proportional_control=self.proportional.get())
+        MyoMouse(velocity=50, proportional_control=self.proportional.get())
 
     def launch_training(self):
         self.window.destroy()
         # Launch training ui
-        training_ui = ScreenGuidedTraining()
-        training_ui.download_gestures([1,2,3,4,5], "classes/")
-        training_ui.launch_training(self.odh, 4, 2, "classes/", "data/", 1)
+        gui_args = {'media_folder': 'images/','data_folder': 'data/','num_reps': 3,'rep_time': 3,'rest_time': 1,'auto_advance': True }
+        training_ui = GUI(self.odh, args=gui_args, width=600, height=600, gesture_width=200, gesture_height=200)
+        training_ui.download_gestures([1,2,3,4,5], "images/")
+        training_ui.start_gui()
         self.initialize_ui()
 
     def set_up_classifier(self):
@@ -59,19 +58,13 @@ class Menu:
 
         # Step 1: Parse offline training data
         dataset_folder = 'data/'
-        classes_values = ["0","1","2","3","4"]
-        classes_regex = make_regex(left_bound = "_C_", right_bound=".csv", values = classes_values)
-        reps_values = ["0", "1", "2", "3"]
-        reps_regex = make_regex(left_bound = "R_", right_bound="_C_", values = reps_values)
-        dic = {
-            "reps": reps_values,
-            "reps_regex": reps_regex,
-            "classes": classes_values,
-            "classes_regex": classes_regex
-        }
+        regex_filters = [
+            RegexFilter(left_bound = "C_", right_bound="_R", values = ["0","1","2","3","4"], description='classes'),
+            RegexFilter(left_bound = "R_", right_bound="_emg.csv", values = ["0", "1", "2"], description='reps'),
+        ]
 
         offline_dh = OfflineDataHandler()
-        offline_dh.get_data(folder_location=dataset_folder, filename_dic=dic, delimiter=",")
+        offline_dh.get_data(dataset_folder, regex_filters, delimiter=",")
         train_windows, train_metadata = offline_dh.parse_windows(WINDOW_SIZE, WINDOW_INCREMENT)
 
         # Step 2: Extract features from offline data
@@ -85,8 +78,8 @@ class Menu:
         data_set['training_labels'] = train_metadata['classes']
 
         # Step 4: Create the EMG Classifier
-        o_classifier = EMGClassifier()
-        o_classifier.fit(model="LDA", feature_dictionary=data_set)
+        o_classifier = EMGClassifier(model="LDA")
+        o_classifier.fit(feature_dictionary=data_set)
         o_classifier.add_velocity(train_windows=train_windows, train_labels=train_metadata['classes'])
         o_classifier.add_rejection(0.90)
 
@@ -96,7 +89,7 @@ class Menu:
 
     def on_closing(self):
         # Clean up all the processes that have been started
-        self.odh.stop_listening()
+        self.streamer.terminate()
         self.window.destroy()
 
 if __name__ == "__main__":
